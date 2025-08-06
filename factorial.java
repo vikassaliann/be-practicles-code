67bf5572ee13fd2e9464180306fd41de09610def
vikas Salian <vikkup261@gmail.com>
2025-08-06 10:34:50 +0530

    second commit

--- factorial.java
+++ factorial.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter a number: ");
-        // int num = sc.nextInt();
-        int num=4;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        // int num=4;
         int factorial = 1;
         for (int i = 1; i <= num; i++) {
             factorial *= i;
